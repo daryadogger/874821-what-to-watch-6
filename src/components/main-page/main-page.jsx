7d658650ff81ae149b96e51c9f677f45ecb18367
Daria Zhikhareva <zhihareva.d@mail.ru>
2021-03-04 19:35:27 +0700

    Делает main-page-view

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,15 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import CardsList from '../cards-list/cards-list';
 import mainPageProps from './main-page.prop';
-import GenresList from '../genres-list/genres-list';
-import {updateGenre} from '../../api/use-film-list';
-import BtnShowMore from '../btn-show-more/btn-show-more';
+import MainPageView from '../main-page/main-page-view';
 import {loadFilms} from '../../mocks/server-data';
+import {updateGenre} from '../../api/use-film-list';
 
 
 const MainPage = (props) => {
   const {promoCard} = props;
-  const {name, genre, src, year} = promoCard;
 
   const MAX_COUNT_OF_FILMS = 8;
   const [count, setCount] = useState(MAX_COUNT_OF_FILMS);
@@ -35,87 +32,7 @@ const MainPage = (props) => {
 
   return <>
 
-    <section className="movie-card">
-      <div className="movie-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-      </div>
-
-      <h1 className="visually-hidden">WTW</h1>
-
-      <header className="page-header movie-card__head">
-        <div className="logo">
-          <a className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="user-block">
-          <div className="user-block__avatar">
-            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-          </div>
-        </div>
-      </header>
-
-      <div className="movie-card__wrap">
-        <div className="movie-card__info">
-          <div className="movie-card__poster">
-            <img src={src} alt={name} width="218" height="327" />
-          </div>
-
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{name}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{year}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list movie-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <div className="page-content">
-      <section className="catalog">
-        <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-        <GenresList genre={filmGenre} setGenre={setFilmGenre} />
-
-        <CardsList genre={filmGenre} count={count} />
-
-        <BtnShowMore onShowMore={onShowMore} isHidden={isHidden} />
-
-      </section>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
+    <MainPageView promoCard={promoCard} filmGenre={filmGenre} setFilmGenre={setFilmGenre} count={count} onShowMore={onShowMore} isHidden={isHidden} />
 
   </>;
 };
