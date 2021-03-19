164af3f7834e4bbdcba74f5962585aa3f7353360
Daria Zhikhareva <zhihareva.d@mail.ru>
2021-03-19 21:01:00 +0700

    Правит компонент Card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import React, {useCallback} from 'react';
+import {Redirect, useHistory} from 'react-router-dom';
 import cardProps from './card.prop';
 import CardView from './card-view';
 import VideoPlayer from '../video-player/video-player';
 import {shallowEqual, useSelector} from 'react-redux';
+
+// to storage ?
+const selectFilmForCard = (FILMS, id) => {
+  const found = FILMS.films.find((el) => el.id === id);
+  if (typeof (found) === `undefined`) {
+    return undefined;
+  }
+  const {name, previewVideoLink, previewImage} = found;
+  return {name, previewVideoLink, previewImage};
+};
+
+const useSelectFilmForCard = (id) => useSelector(({FILMS}) => selectFilmForCard(FILMS, id), shallowEqual);
 
 const Card = (props) => {
   const {id, to, onActiveFilmChange, isActive} = props;
 
   const history = useHistory();
 
-  const film = useSelector(({FILMS}) => FILMS.films.find((el) => el.id === id), shallowEqual);
-  if (film === null) {
-    history.push(`/not-found-page`);
-  }
+  const film = useSelectFilmForCard(id);
 
-  const {name, previewVideoLink, previewImage} = film;
-
-  const handleClick = () => {
+  const handleCardClick = useCallback(() => {
     history.push(`/films/${id}`);
-  };
+  }, [id]);
 
-  const handleMouseEnter = () => {
+  const handleMouseEnter = useCallback(() => {
     onActiveFilmChange(id);
-  };
+  }, [id]);
 
-  const handleMouseLeave = () => {
+  const handleMouseLeave = useCallback(() => {
     onActiveFilmChange(null);
-  };
+  }, []);
+
+
+  if (film === null) {
+    return <Redirect to={`/not-found-page`} />;
+  }
 
   return (
 
-    <CardView film={name} to={to} onClickHandler={handleClick} onMouseLeaveHandler={handleMouseLeave} onMouseEnterHandler={handleMouseEnter} id={id} >
-      <VideoPlayer isActive={isActive} src={previewVideoLink} posterImage={previewImage} width={280} height={175} alt={name} />
+    <CardView film={film.name} to={to} onClickHandler={handleCardClick} onMouseLeaveHandler={handleMouseLeave} onMouseEnterHandler={handleMouseEnter} id={id} >
+      <VideoPlayer isActive={isActive} src={film.previewVideoLink} posterImage={film.previewImage} width={280} height={175} alt={film.name} />
     </CardView>
 
   );
@@ -40,4 +52,4 @@ const Card = (props) => {
 
 Card.propTypes = cardProps;
 
-export default React.memo(Card);
+export default Card;
